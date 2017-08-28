310dcff71ac64c4698025047513bad34c18224b9
Alexadnru Guzun <alexandru.guzun@sky.uk>
2017-08-28 16:47:05 +0100

    Added support for SSL, based on https://github.com/hashicorp/vault/blob/b9711d040f4dcf00b95619d323d6bbfeae9ebf76/plugins/database/mongodb/connection_producer.go#L101 implementation

--- infrastructure/db/db.go
+++ infrastructure/db/db.go
@@ -1,16 +1,101 @@
 package db
 
 import (
+	"crypto/tls"
+	"errors"
 	"gopkg.in/mgo.v2"
+	"net"
+	"net/url"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
+/*
+Source : https://github.com/hashicorp/vault/blob/master/plugins/database/mongodb/connection_producer.go#L101
+*/
+func parseMongoURL(rawURL string) (*mgo.DialInfo, error) {
+	url, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, err
+	}
+
+	info := mgo.DialInfo{
+		Addrs:    strings.Split(url.Host, ","),
+		Database: strings.TrimPrefix(url.Path, "/"),
+		Timeout:  10 * time.Second,
+	}
+
+	if url.User != nil {
+		info.Username = url.User.Username()
+		info.Password, _ = url.User.Password()
+	}
+
+	query := url.Query()
+	for key, values := range query {
+		var value string
+		if len(values) > 0 {
+			value = values[0]
+		}
+
+		switch key {
+		case "authSource":
+			info.Source = value
+		case "authMechanism":
+			info.Mechanism = value
+		case "gssapiServiceName":
+			info.Service = value
+		case "replicaSet":
+			info.ReplicaSetName = value
+		case "maxPoolSize":
+			poolLimit, err := strconv.Atoi(value)
+			if err != nil {
+				return nil, errors.New("bad value for maxPoolSize: " + value)
+			}
+			info.PoolLimit = poolLimit
+		case "ssl":
+			// Unfortunately, mgo doesn't support the ssl parameter in its MongoDB URI parsing logic, so we have to handle that
+			// ourselves. See https://github.com/go-mgo/mgo/issues/84
+			ssl, err := strconv.ParseBool(value)
+			if err != nil {
+				return nil, errors.New("bad value for ssl: " + value)
+			}
+			if ssl {
+				info.DialServer = func(addr *mgo.ServerAddr) (net.Conn, error) {
+					return tls.Dial("tcp", addr.String(), &tls.Config{})
+				}
+			}
+		case "connect":
+			if value == "direct" {
+				info.Direct = true
+				break
+			}
+			if value == "replicaSet" {
+				break
+			}
+			fallthrough
+		default:
+			return nil, errors.New("unsupported connection URL option: " + key + "=" + value)
+		}
+	}
+
+	return &info, nil
+}
+
 func GetMongoSession() *mgo.Session {
-	mongoUri := os.Getenv("MONGO_URI")
-	if len(mongoUri) <= 0 {
+	uri := os.Getenv("MONGO_URI")
+	if len(uri) <= 0 {
 		panic("MONGO_URI variable not set!")
 	}
-	session, err := mgo.Dial(mongoUri)
+
+	info, err := parseMongoURL(uri)
+	if err != nil {
+		panic(err)
+	}
+
+	session, err := mgo.DialWithInfo(info)
+
 	if err != nil {
 		panic(err)
 	}
